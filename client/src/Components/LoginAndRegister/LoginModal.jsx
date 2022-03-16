3e99953448032abc1dcc3325983e90331b0ad7f6
luigi3357 <luigi3579@gmail.com>
2022-03-16 06:25:55 -0300

    register conect and component login

--- client/src/Components/LoginAndRegister/LoginModal.jsx
+++ client/src/Components/LoginAndRegister/LoginModal.jsx
@@ -13,7 +13,7 @@ import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import '../../index.css';
 import { useDispatch, useSelector } from "react-redux";
-import register, { getAllUsers } from '../../REDUX/actions/action';
+import { getAllUsers } from '../../REDUX/actions/action';
 
 
 
