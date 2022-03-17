e6831a38003b878337d7b4fde980e795090fdf24
lucasduran33 <lucasaduranlopez@gmail.com>
2022-03-16 22:24:18 -0300

    forms-validations

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Form from '../src/Components/Form/Form'
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<algo/>}/>        
+        <Route path="/" element={<Form/>}/>        
       </Routes>
     </Router>    
   );
