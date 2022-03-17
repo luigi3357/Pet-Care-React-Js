add02f291fd58f425b78420558dc2e48279d1841
Darian Esquivel <darianesquivelf@gmail.com>
2022-03-17 12:01:44 -0300

    se acomodaron los margenes del nav

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -5,27 +5,27 @@ import { Avatar } from "primereact/avatar";
 
 export const NavBar = () => {
   return (
-    <div className="flex justify-content-end  w-50">
+    <div className="flex justify-content-end  w-50 mb-3">
       <Link to="/">
         <Button
           label="Home"
           icon="pi pi-fw pi-home"
-          className="p-button-rounded p-button-info p-button-outlined"
+          className="p-button-rounded p-button-info p-button-outlined ml-1 mr-1"
         />
       </Link>
       <div className="flex">
         <Link to="/Register">
-          <Button label="Registrarse" className="p-button-rounded" />
+          <Button label="Registrarse" className="p-button-rounded ml-1 mr-1" />
         </Link>
         <Link to="/Login">
           <Button
             label="Iniciar Sesion"
-            className="p-button-secondary p-button-rounded"
+            className="p-button-secondary p-button-rounded ml-1 mr-1-"
           />
         </Link>
       </div>
 
-      <Avatar label="P" shape="circle" size="large" />
+      <Avatar label="P" shape="circle" size="large" className="ml-1 mr-1" />
     </div>
   );
 };
