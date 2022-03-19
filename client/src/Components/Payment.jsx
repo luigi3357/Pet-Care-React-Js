20fb5091f6ee0810dfac2e4db93121c082052539
julio-hdz <jchj1793@gmail.com>
2022-03-19 20:01:41 -0300

    moviendo componentes de pag a components

--- client/src/Components/Payment.jsx
+++ client/src/Components/Payment.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-import ClientView from "../Components/ClientView";
-import KeeperView from "../Components/KeeperView";
+import ClientView from "./ClientView";
+import KeeperView from "./KeeperView";
 import { changeBookingStatus, fetchCheckOutDetails, postPayment } from "../REDUX/actions/action";
 
 export function Payment() {
